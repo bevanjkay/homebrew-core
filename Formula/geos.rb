55a604d3f46d1dc6188e2324fd2584785bc6bdc0
Jonathan Chang <me@jonathanchang.org>
2021-10-21 13:53:08 -0700

    geos: improve test

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -27,6 +27,34 @@ class Geos < Formula
   end
 
   test do
-    system "#{bin}/geos-config", "--libs"
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdarg.h>
+      #include <geos_c.h>
+      static void geos_message_handler(const char* fmt, ...) {
+          va_list ap;
+          va_start(ap, fmt);
+          vprintf (fmt, ap);
+          va_end(ap);
+      }
+      int main() {
+          initGEOS(geos_message_handler, geos_message_handler);
+          const char* wkt_a = "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))";
+          const char* wkt_b = "POLYGON((5 5, 15 5, 15 15, 5 15, 5 5))";
+          GEOSWKTReader* reader = GEOSWKTReader_create();
+          GEOSGeometry* geom_a = GEOSWKTReader_read(reader, wkt_a);
+          GEOSGeometry* geom_b = GEOSWKTReader_read(reader, wkt_b);
+          GEOSGeometry* inter = GEOSIntersection(geom_a, geom_b);
+          GEOSWKTWriter* writer = GEOSWKTWriter_create();
+          GEOSWKTWriter_setTrim(writer, 1);
+          char* wkt_inter = GEOSWKTWriter_write(writer, inter);
+          printf("Intersection(A, B): %s\\n", wkt_inter);
+          return 0;
+      }
+    EOS
+    cflags = shell_output("#{bin}/geos-config --cflags").split
+    libs = shell_output("#{bin}/geos-config --clibs").split
+    system ENV.cc, *cflags, "test.c", *libs
+    assert_match "POLYGON ((10 10, 10 5, 5 5, 5 10, 10 10))", shell_output("./a.out")
   end
 end
