225e8fb220ff3e20995adbf3f44bfbbeca4b9012
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-12-20 08:15:01 +0100

    node: disable corepack

--- Formula/node.rb
+++ Formula/node.rb
@@ -75,6 +75,7 @@ class Node < Formula
     args = %W[
       --prefix=#{prefix}
       --without-npm
+      --without-corepack
       --with-intl=system-icu
       --shared-libuv
       --shared-nghttp2
