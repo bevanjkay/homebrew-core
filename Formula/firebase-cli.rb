26f5deccd8d7bb962666cb9d4f079ac36dc5f117
Bevan Kay <email@bevankay.me>
2022-11-05 14:54:37 +1100

    restore term-size dependency

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -21,10 +21,23 @@ class FirebaseCli < Formula
   depends_on "node"
 
   uses_from_macos "expect" => :test
+  on_macos do
+    depends_on "macos-term-size"
+  end
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    term_size_vendor_dir = libexec/"lib/node_modules/firebase-tools/node_modules/term-size/vendor"
+    term_size_vendor_dir.rmtree # remove pre-built binaries
+
+    if OS.mac?
+      macos_dir = term_size_vendor_dir/"macos"
+      macos_dir.mkpath
+      # Replace the vendored pre-built term-size with one we build ourselves
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
+    end
   end
 
   test do
